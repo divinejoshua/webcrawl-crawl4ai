c4c43c3602fa8553cec516172694cf499c2de416
divinejoshua <divineuche399@gmail.com>
2025-04-03 19:15:37 +0100

    Setup crawl4ai

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig
+from crawl4ai import *
 
 async def main():
     async with AsyncWebCrawler() as crawler:
         result = await crawler.arun(
-            url="https://www.example.com",
+            url="https://www.nbcnews.com/business",
         )
-        print(result.markdown[:300])  # Show the first 300 characters of extracted text
+        print(result.markdown)
 
 if __name__ == "__main__":
     asyncio.run(main())
